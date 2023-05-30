d028b4c5aec49abf4d033cee81db9f95394407dc
Drew Anello <drew.anello@gmail.com>
2023-05-29 20:12:46 -0400

    adds styling to resume

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,10 +1,20 @@
+
+import Image from "next/image"
+import emaresume from "../../../public/assets/website/emaresume.jpg"
+
 function page() {
     return (
-        <main className="mb-10">
-            <div className="mt-20">
-                This is your resume
-            </div>
-        </main>
+        <div className="mt-20 mb-10">
+
+            <Image src={emaresume}
+                alt="Ema's Resume" />
+            <br></br>
+            <div className="flex ">
+                <div className="justify-center border-8 border-green-400">
+                    <a className="text-xl font-extrabold text-orange-500 bg-green-400 border-4 border-green-400" href="./emaResume.pdf" download="Ema's Resume">Download My Resume</a>
+                </div>
+            </div >
+        </div >
     )
 }
 
