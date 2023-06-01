17ed0c828fcd94ba1c246bf1ccd1a2bd6aecf07a
Drew Anello <drew.anello@gmail.com>
2023-06-01 16:52:07 -0400

    fixes nav adds name

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -2,27 +2,29 @@ import Link from "next/link"
 function Nav() {
     return (
         <div className="mt-3 font-serif text-xl">
-            <ul className="flex justify-around">
-                <li>
+            <ul className="flex">
+                <li className="pl-10 mr-auto text-2xl font-semibold">
                     <Link href='/'>
-                        Home
+                        Ema Zivkovic
                     </Link>
                 </li>
-                <li>
-                    <Link href='/resume'>
-                        Resume
-                    </Link>
-                </li>
-                <li>
-                    <Link href='/choreography'>
-                        Choreography
-                    </Link>
-                </li>
-                <li>
-                    <Link href='/contact'>
-                        Contact
-                    </Link>
-                </li>
+                <div className="flex justify-between flex-auto pl-4">
+                    <li>
+                        <Link href='/resume'>
+                            Resume
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href='/choreography'>
+                            Choreography
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href='/contact'>
+                            Contact
+                        </Link>
+                    </li>
+                </div>
             </ul>
         </div>
     )
