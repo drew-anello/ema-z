695a2fa193bb8f31f1dff7fd9c55167e4dff8ee5
Drew Anello <drew.anello@gmail.com>
2023-06-18 22:26:28 -0400

    adds images to resume page

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,21 +1,28 @@
-
-import Image from "next/image"
-import emaresume from "../../../public/assets/website/emaresume.jpg"
+import Image from "next/image";
+import emaresume from "../../../public/assets/website/emaresume.jpg";
+import emresumepic from "../../../public/assets/website/emresumepic.jpg";
 
 function page() {
-    return (
-        <div className="mt-20 mb-10">
-
-            <Image src={emaresume}
-                alt="Ema's Resume" />
-            <br></br>
-            <div className="flex ">
-                <div className="justify-center border-8 border-green-400">
-                    <a className="text-xl font-extrabold text-orange-500 bg-green-400 border-4 border-green-400" href="./emaResume.pdf" download="Ema's Resume">Download My Resume</a>
-                </div>
-            </div >
-        </div >
-    )
+  return (
+    <div className="mt-10 mb-10">
+      <div className="grid grid-cols-1 gap-4 md:gap-1 md:w-fit md:grid-cols-2">
+        <Image src={emresumepic} alt="Headshot of Ema" className="md:w-1/2" />
+        <Image src={emaresume} alt="Ema's Resume" />
+      </div>
+      <br></br>
+      <div className="flex mt-4 ">
+        <div className="justify-center border-8 border-green-400">
+          <a
+            className="text-xl font-extrabold text-orange-500 bg-green-400 border-4 border-green-400"
+            href="./emaResume.pdf"
+            download="Ema's Resume"
+          >
+            Download My Resume
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default page
+export default page;
