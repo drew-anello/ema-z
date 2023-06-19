6e35417e083020ec5af7ad677b74ba0c3cdf8f4e
Drew Anello <drew.anello@gmail.com>
2023-06-18 21:58:59 -0400

    makes nav bar responsive

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -1,33 +1,26 @@
-import Link from "next/link"
+import Link from "next/link";
+
 function Nav() {
-    return (
-        <div className="mt-3 font-serif text-xl">
-            <ul className="flex">
-                <li className="pl-10 mr-auto text-2xl font-semibold">
-                    <Link href='/'>
-                        Ema Zivkovic
-                    </Link>
-                </li>
-                <div className="flex justify-between flex-auto pl-4">
-                    <li>
-                        <Link href='/resume'>
-                            Resume
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href='/choreography'>
-                            Choreography
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href='/contact'>
-                            Contact
-                        </Link>
-                    </li>
-                </div>
-            </ul>
+  return (
+    <div className="mt-3 font-serif text-xl md:text-center">
+      <ul className="flex flex-wrap md:flex-no-wrap">
+        <li className="pl-4 mr-auto text-2xl font-semibold md:pl-10">
+          <Link href="/">Ema Zivkovic</Link>
+        </li>
+        <div className="flex flex-col md:flex-row md:justify-between md:flex-auto">
+          <li className="mt-2 md:mt-0">
+            <Link href="/resume">Resume</Link>
+          </li>
+          <li className="mt-2 md:mt-0 md:ml-4">
+            <Link href="/choreography">Choreography</Link>
+          </li>
+          <li className="mt-2 md:mt-0 md:ml-4">
+            <Link href="/contact">Contact</Link>
+          </li>
         </div>
-    )
+      </ul>
+    </div>
+  );
 }
 
-export default Nav
+export default Nav;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,24 @@
-import './globals.css'
-import Nav from './components/Nav'
-import { Inter } from 'next/font/google'
-const inter = Inter({ subsets: ['latin'] })
+import "./globals.css";
+import Nav from "./components/Nav";
+import { Inter } from "next/font/google";
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Ema Zivkovic',
-  description: 'Actor and Choreographer based in NYC',
-}
+  title: "Ema Zivkovic",
+  description: "Actor and Choreographer based in NYC",
+};
 
 export default function RootLayout({
-
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{<Nav />}{children}
-
+      <body className={inter.className}>
+        {<Nav />}
+        {children}
       </body>
     </html>
-  )
+  );
 }
