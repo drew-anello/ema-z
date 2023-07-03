8aeb9260f32e3da1d825ba1d535da1d5e2c6a577
Drew Anello <drew.anello@gmail.com>
2023-07-03 13:13:04 -0400

    updates choreo page

--- src/app/choreography/page.tsx
+++ src/app/choreography/page.tsx
@@ -1,5 +1,30 @@
 function page() {
-  return <div>choreography</div>;
+  return (
+    <div className="mt-10 ">
+      <h1 className="m-4 text-2xl">CHOREO BIO</h1>
+      <p className="m-4 text-left">
+        Ema has been choreographing in New York and regionally for the last 5
+        years. Ema is thrilled to be continuously involved in pre-production
+        with choreographer/director Katie Spelman (Moulin Rouge! The Musical:
+        New York) on shows such as Water for Elephants, Cabaret, The Who's
+        Tommy, and more to come. Some choreography credits include: Stacy & Mia
+        by SMJ (National Theatre Institute Residency; The Tank), Late Night
+        Cabaret (Williamstown Theatre Festival), Cabaret by Kander & Ebb (Sarah
+        Lawrence College), and more. Latest intimacy direction credits in
+        theatre include: Small Mouth Sounds by Bess Wohl (University of
+        Pennsylvania) and War & Play by Danielle Levsky (Lincoln Center). To
+        request an updated resume or for any booking inquiries,{" "}
+        <a
+          className="text-purple-300 text-bold"
+          href="mailto:emazivkovic17@gmail.com
+"
+        >
+          click here
+        </a>
+        .
+      </p>
+    </div>
+  );
 }
 
 export default page;
