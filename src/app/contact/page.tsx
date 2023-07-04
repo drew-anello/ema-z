ce9737f25987b0ce64c015e23b5b029fd349b63f
Drew Anello <drew.anello@gmail.com>
2023-07-03 23:00:48 -0400

    adds contact page content

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,19 @@
 function page() {
-  return <div></div>;
+  return (
+    <div className="flex flex-col items-center">
+      <h1 className="mt-8 text-3xl font-bold">Contact Me</h1>
+      <p className="mt-4 text-xl">
+        For all inquiries and bookings, please email{" "}
+        <a
+          href="mailto:emazivkovic17@gmail.com"
+          className="text-blue-500 hover:underline"
+        >
+          emazivkovic17@gmail.com
+        </a>
+        .
+      </p>
+    </div>
+  );
 }
 
 export default page;
